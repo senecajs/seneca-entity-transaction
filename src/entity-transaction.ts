e25a67391be4cd63ea2246ed95e11168f9aac5c9
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2023-06-15 00:35:15 +0300

    Rephrase notes as questions-answers

--- src/entity-transaction.ts
+++ src/entity-transaction.ts
@@ -43,21 +43,38 @@ class TrxApi<TCtx> {
   }
 
   async start(): Promise<Seneca> {
-    // NOTE: If a transaction already exists, we __must__ nonetheless invoke
+    // QUESTION: If a transaction already exists, why do we invoke the startTrx
+    // hook anyway?
+    //
+    // ANSWER: If a transaction already exists, we __must__ nonetheless invoke
     // the startTrx hook because:
     // - client's strategy may utilize a db client which supports nested transactions
     // - client's strategy may implement custom logic to handle nested transactions
 
     const ctx: TCtx = await this.strategy.startTrx(this.seneca)
 
+    // QUESTION: Why not store the context directly, i.e. why wrap it in
+    // a Trx object?
+    //
+    // ANSWER: Further down the line we may want to add additional state to
+    // transactions. Wrapping user transaction contexts in an object will make
+    // it easy for us to add additional state in the future, without breaking
+    // existing seneca-store integrations or users.
+    //
     const trx: Trx<TCtx> = {
       ctx
     }
 
-    // NOTE: We __must__ return a delegate here, because some db clients either
-    // implement an adapter to simulate nested transactions (e.g. knex with
-    // the mysql2 driver) or support nested transactions directly. Therefore,
-    // a nested transaction's handle, too, must be stored.
+    // QUESTION: If a transaction already exists, can't we do without creating
+    // a Seneca delegate, and just return the current Seneca instance instead?
+    //
+    // ANSWER: No. We __must__ return a delegate here, because some db clients
+    // either implement an adapter to simulate nested transactions (e.g. knex
+    // with the mysql2 driver) or support nested transactions directly.
+    // Therefore, a nested transaction's handle, too, must be stored.
+    //
+    // In other words, nested transactions, if a seneca-store's db supports
+    // them, have their own state, which, too, must be stored.
     //
     const seneca_trx: Seneca = this.seneca.delegate(null, {
       custom: {
@@ -71,10 +88,11 @@ class TrxApi<TCtx> {
   }
 
   async commit() {
+    await this.strategy.commitTrx(this.seneca)
+
     // QUESTION: Why can't we implement support for nested transactions in this
     // plugin, instead of leaving it up to db-store plugins and their strategies
     // to implement them?
-    //
     //
     // ANSWER: TL;DR, here's an example. Assume a db-store plugin uses a db
     // whose driver supports nested transactions.
@@ -96,15 +114,24 @@ class TrxApi<TCtx> {
     // In the code snippet above, the expected result is for Bob to be saved
     // to the db, but not Alice. The db client does that book-keeping for us
     // for free.
+    //
+    // We cannot rely on seneca-entity to keep track of operations to manage
+    // nested transactions either, because:
+    //
+    // - some seneca stores support .native() which exposes direct access
+    //   to the store's db client to users, bypassing seneca-entity completely
+    //
+    // - some users have hybrid setups where both seneca-entity and their
+    //   own code access a single db
 
-    await this.strategy.commitTrx(this.seneca)
 
     // QUESTION: Why are we are not null-ifying completed transactions?
     //
     // ANSWER: We are not null-ifying completed transactions because we want
     // to leave it up to a client's strategy to handle reuse of trx instances.
-    // This plugin is just a thin overlay between db-store plugins and
-    // Seneca users.
+    //
+    // This plugin is just a thin overlay between db-store plugins and Seneca
+    // users.
   }
 
   async rollback() {
@@ -113,6 +140,23 @@ class TrxApi<TCtx> {
 
   getContext(): Option<TCtx> {
     return Intern.getContext(this.seneca)
+
+    // QUESTION: What's up with the weird Option<TCtx> type - can't you just
+    // return the context?
+    //
+    // ANSWER: If getContext() returned null or the context, users would have
+    // no way of telling whether null meant there was no pending transaction,
+    // or whether the context itself was set to null.
+
+
+    // QUESTION: Why not return the Trx object instead of just the context?
+    //
+    // ANSWER: Further down the line we may want to add additional state to
+    // transactions. We do not want users to depend on that state.
+    //
+    // If we decide to expose that state to users, we may easily extend this
+    // API (e.g. getTransaction) without breaking existing seneca-store
+    // integrations or users.
   }
 }
 
